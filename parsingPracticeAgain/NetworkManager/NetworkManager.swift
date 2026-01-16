07a4794349fc1c92f96f4b9865bb5d8dead0ab97
AtesiT <atesit@mail.ru>
2026-01-16 13:05:00 +0300

    MADE: func to sendData

--- parsingPracticeAgain/NetworkManager/NetworkManager.swift
+++ parsingPracticeAgain/NetworkManager/NetworkManager.swift
@@ -26,7 +26,27 @@ final class NetworkManager: UIViewController {
             DispatchQueue.main.async {
                 completion(.success(imageData))
             }
-            
         }
+    }
+    
+    private func toSendData(with parameters: [String:Any], to url: URL, completion: @escaping(Result<Any,MyErrors>) -> Void) {
+        let serializedData = try? JSONSerialization.data(withJSONObject: parameters)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = serializedData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data, let response else {
+                print(error?.localizedDescription ?? "Nothing")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                completion(.success(json))
+            } catch {
+                completion(.failure(.Error))
+            }
+        }.resume()
     }
 }
