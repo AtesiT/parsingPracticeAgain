fd1011edd20a02ef5f92339a94fc1831fb635dde
AtesiT <atesit@mail.ru>
2026-01-17 12:43:00 +0300

    ADD: set button on VC and set action parse data in console

--- parsingPracticeAgain/ViewControllers/ViewController.swift
+++ parsingPracticeAgain/ViewControllers/ViewController.swift
@@ -1,12 +1,62 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    let networkManager = NetworkManager.shared
+    
+    private lazy var parseJSONButton: UIButton = {
+        let parseJSONButton = FilledButtonFactory(
+            title: "Take Data",
+            color: .red,
+            action: UIAction { [unowned self] _ in
+                parseJSONFromWeb()
+            }
+        )
+        return parseJSONButton.createButton()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
+        setElements(parseJSONButton)
+        setConstraints()
     }
 
 
+}
+
+// MARK: - Parse Funcs
+private extension ViewController {
+    func parseJSONFromWeb() {
+        networkManager.toParseData(from: Links.dataTakeURL.url) { result in
+            switch result {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+//  MARK: - Constraints and Set Buttons
+private extension ViewController {
+    func setElements(_ subView: UIView...) {
+        subView.forEach{ theElement in
+            view.addSubview(theElement)
+        }
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            parseJSONButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            parseJSONButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            parseJSONButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+}
+
+#Preview {
+    ViewController()
 }
 
